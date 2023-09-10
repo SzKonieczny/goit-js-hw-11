e7357e53cf5da3a9fdece1c3110abd776059c8f6
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-09-10 23:38:37 +0200

    index.js, index.hatml updated, index.css added

--- src/js/index.js
+++ src/js/index.js
@@ -60,6 +60,141 @@
 //   }
 // });
 
+// import axios from 'axios';
+// import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const searchForm = document.querySelector('.search-form');
+// const gallery = document.querySelector('.gallery');
+// const loadMoreBtn = document.querySelector('.load-more');
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionDelay: 250,
+//   captionsData: 'alt',
+// });
+
+// const PIXIBAY_API_URL = 'https://pixabay.com/api/';
+// const API_KEY = '39350570-0225d4429ca2bb8d9989d6d5b';
+// const PER_PAGE = 50;
+// let currentPage = 1;
+
+// searchForm.addEventListener('input', event => {
+//   const searchValue = event.target.value;
+//   localStorage.setItem('search item', searchValue);
+// });
+
+// searchForm.addEventListener('submit', handleSearch);
+
+// loadMoreBtn.addEventListener('click', loadMore);
+
+// async function handleSearch(event) {
+//   event.preventDefault();
+//   const searchValue = event.target.searchQuery.value.trim();
+
+//   if (searchValue === '') {
+//     Notiflix.Notify.info('Please enter a search query.');
+//     return;
+//   }
+
+//   currentPage = 1; // Reset currentPage when performing a new search.
+//   await fetchAndDisplayImages(searchValue, currentPage);
+// }
+
+// async function loadMore() {
+//   const searchValue = localStorage.getItem('search item');
+//   currentPage++;
+
+//   try {
+//     const loadMoreMarkup = await fetchImages(searchValue, currentPage);
+//     if (loadMoreMarkup === null) {
+//       // footer.style.display = 'none';
+//       loadMoreBtn.style.display = 'none';
+//       Notiflix.Notify.info(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//       return;
+//     }
+//     gallery.innerHTML += loadMoreMarkup; // Dodajemy nowe zdjęcia do istniejącej zawartości galerii
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// async function fetchAndDisplayImages(searchValue, currentPage) {
+//   try {
+//     const imagesMarkup = await fetchImages(searchValue, currentPage);
+//     gallery.innerHTML = imagesMarkup;
+//     loadMoreBtn.style.display = 'block';
+//     //footer.style.display = 'flex';
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// async function fetchImages(searchValue, currentPage) {
+//   let markupGallery = '';
+//   let params = new URLSearchParams({
+//     key: API_KEY,
+//     q: searchValue,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     per_page: PER_PAGE,
+//     page: currentPage,
+//   });
+
+//   try {
+//     const response = await axios.get(`${PIXIBAY_API_URL}?${params}`);
+//     const imagesArray = response.data.hits;
+//     if (imagesArray.length === 0) {
+//       Notiflix.Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//       return null;
+//     }
+
+//     markupGallery = imagesArray
+//       .map(image => {
+//         return `
+//         <div class="photo-card">
+//           <div class ="thumb">
+//             <img class="img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+//           </div>
+//           <div class="info">
+//             <p class="info-item">
+//               <b>Likes</b>
+//               ${image.likes}
+//             </p>
+//             <p class="info-item">
+//               <b>Views</b>
+//               ${image.views}
+//             </p>
+//             <p class="info-item">
+//               <b>Comments</b>
+//               ${image.comments}
+//             </p>
+//             <p class="info-item">
+//               <b>Downloads</b>
+//               ${image.downloads}
+//             </p>
+//           </div>
+//         </div>
+//       `;
+//       })
+//       .join('');
+
+//     return markupGallery;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
+
+// // Initial hiding of loadMoreBtn
+// loadMoreBtn.style.display = 'none';
+// // loadMoreBtn.classList.add('is-hidden');
+
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
@@ -67,15 +202,15 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
+const loadBtn = document.querySelector('.load-more');
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
 
-const PIXIBAY_API_URL = 'https://pixabay.com/api/';
-const API_KEY = '39350570-0225d4429ca2bb8d9989d6d5b';
+const PIXIBAY_URL = 'https://pixabay.com/api/';
+const KEY_PIXI = '39267402-49695b078cc30e5676dab55fe';
 const PER_PAGE = 50;
 let currentPage = 1;
 
@@ -86,7 +221,7 @@ searchForm.addEventListener('input', event => {
 
 searchForm.addEventListener('submit', handleSearch);
 
-loadMoreBtn.addEventListener('click', loadMore);
+loadBtn.addEventListener('click', loadMore);
 
 async function handleSearch(event) {
   event.preventDefault();
@@ -109,7 +244,7 @@ async function loadMore() {
     const loadMoreMarkup = await fetchImages(searchValue, currentPage);
     if (loadMoreMarkup === null) {
       // footer.style.display = 'none';
-      loadMoreBtn.style.display = 'none';
+      loadBtn.style.display = 'none';
       Notiflix.Notify.info(
         "We're sorry, but you've reached the end of search results."
       );
@@ -125,7 +260,7 @@ async function fetchAndDisplayImages(searchValue, currentPage) {
   try {
     const imagesMarkup = await fetchImages(searchValue, currentPage);
     gallery.innerHTML = imagesMarkup;
-    loadMoreBtn.style.display = 'block';
+    loadBtn.style.display = 'block';
     //footer.style.display = 'flex';
   } catch (error) {
     console.log(error);
@@ -135,7 +270,7 @@ async function fetchAndDisplayImages(searchValue, currentPage) {
 async function fetchImages(searchValue, currentPage) {
   let markupGallery = '';
   let params = new URLSearchParams({
-    key: API_KEY,
+    key: KEY_PIXI,
     q: searchValue,
     image_type: 'photo',
     orientation: 'horizontal',
@@ -145,7 +280,7 @@ async function fetchImages(searchValue, currentPage) {
   });
 
   try {
-    const response = await axios.get(`${PIXIBAY_API_URL}?${params}`);
+    const response = await axios.get(`${PIXIBAY_URL}?${params}`);
     const imagesArray = response.data.hits;
     if (imagesArray.length === 0) {
       Notiflix.Notify.failure(
@@ -191,5 +326,5 @@ async function fetchImages(searchValue, currentPage) {
   }
 }
 
-// Initial hiding of loadMoreBtn
-loadMoreBtn.style.display = 'none';
+// Initial hiding of loadBtn
+loadBtn.style.display = 'none';
